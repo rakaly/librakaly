225ca59b7c273dbde3127ee6ec12602aa732e521
Nick Babcock <nbabcock19@hotmail.com>
2021-01-25 06:55:09 -0600

    Update rakaly dependencies
    Will correctly melt seed properties as integers instead of dates

--- src/lib.rs
+++ src/lib.rs
@@ -119,7 +119,7 @@ fn _rakaly_eu4_melt(data_ptr: *const c_char, data_len: size_t) -> MeltedBuffer {
     let dp = data_ptr as *const c_uchar;
     let data = unsafe { std::slice::from_raw_parts(dp, data_len) };
     eu4save::melt(&data, eu4save::FailedResolveStrategy::Ignore)
-        .map(|buffer| MeltedBuffer {
+        .map(|(buffer, _tokens)| MeltedBuffer {
             buffer,
             error: None,
         })
@@ -164,7 +164,7 @@ fn _rakaly_ck3_melt(data_ptr: *const c_char, data_len: size_t) -> MeltedBuffer {
     Melter::new()
         .with_on_failed_resolve(FailedResolveStrategy::Ignore)
         .melt(data)
-        .map(|buffer| MeltedBuffer {
+        .map(|(buffer, _tokens)| MeltedBuffer {
             buffer,
             error: None,
         })
@@ -211,7 +211,7 @@ fn _rakaly_imperator_melt(data_ptr: *const c_char, data_len: size_t) -> MeltedBu
     Melter::new()
         .with_on_failed_resolve(FailedResolveStrategy::Ignore)
         .melt(data)
-        .map(|buffer| MeltedBuffer {
+        .map(|(buffer, _tokens)| MeltedBuffer {
             buffer,
             error: None,
         })

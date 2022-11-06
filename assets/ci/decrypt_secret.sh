6e36de7fad67d76487d09a43f429305bd752eebc
Nick Babcock <nbabcock19@hotmail.com>
2022-11-05 21:45:11 -0500

    Update ci token script

--- assets/ci/decrypt_secret.sh
+++ assets/ci/decrypt_secret.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"/../..
-for i in "ck3" "eu4" "imperator" "hoi4"; do
+for i in "ck3" "eu4" "imperator" "hoi4" "vic3"; do
     gpg --batch --yes --decrypt --passphrase="$TOKEN_PASSPHRASE" --output assets/tokens/$i.txt assets/tokens/$i.txt.gpg
 done;

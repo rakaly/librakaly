6702814dac9b3b583f5fa4d61b39e588510d7e0d
Nick Babcock <nbabcock19@hotmail.com>
2024-01-17 18:09:14 -0600

    Update cbindgen and other deps to latest

--- build.rs
+++ build.rs
@@ -17,13 +17,8 @@ fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_path = Path::new(&crate_dir).join("target").join("rakaly.h");
 
-    let config = cbindgen::Config {
-        cpp_compat: true,
-        ..Default::default()
-    };
-
     cbindgen::Builder::new()
-        .with_config(config)
+        .with_cpp_compat(true)
         .with_crate(crate_dir)
         .with_language(cbindgen::Language::C)
         .with_no_includes()
